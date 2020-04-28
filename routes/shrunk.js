5aab0406ceab2006364aa99ef998fbc86ef3638a
Rick Buitrago <rick@elevatestartup.com>
2020-04-28 08:52:41 -0500

    applying redirect to google.com for now

--- routes/shrunk.js
+++ routes/shrunk.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/:shrunkId', function(req, res, next) {
   console.log('Received shrunk URL user request for shrunkId: ' + req.params.shrunkId);
-  res.send('this should be a redirect to whatever value you set for http://shri.nk/' + req.params.shrunkId);
+  res.redirect(301, 'https://www.google.com');
 });
 
 module.exports = router;
