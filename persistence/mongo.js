93ec7895fe89037a7a678602e07f989e7943acf5
Rick Buitrago <rick@elevatestartup.com>
2020-04-28 10:02:00 -0500

    updated persistence

--- persistence/mongo.js
+++ persistence/mongo.js
@@ -1,13 +1,37 @@
-var MongoClient = require('mongodb').MongoClient;
+var mongo = require('mongodb').MongoClient;
 
-var handleConnection = function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+var handleConnection = function(err, dbConnection) {
+    if (err) {
+        throw err;
+    }
+    console.log('Mongo Connection opened successfully');
+
+    
+    var dbInstance = dbConnection.db('shrink');
+    console.log('Got connection to DB, now using "shrink" DB!');
+    var shrunkUrlsCollection = dbInstance.collection('shrunk');
+    console.log('Created collection! ' + shrunkUrlsCollection);
+    
+    var obj = {source: 'http://www.bbc.co.uk', shrunkId: 'amg01'};
+    
+    shrunkUrlsCollection.insertOne(obj, (err, result) => {
+        console.log('wrote ' + obj + ' to collection!');
+    });
 }
 
-var connectAndCreate = function(url) {
-    MongoClient.connect(url, handleConnection);
+var handleInitialization = function (err, dbc) {
+    if (err) {
+        throw err;
+    }
+    dbc.close()    
 }
 
-module.exports = connectAndCreate;
+var init = function(url) {
+    var connectionProperties = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      };
+    mongo.connect(url, connectionProperties, handleInitialization);
+}
+
+module.exports = {init: init};
