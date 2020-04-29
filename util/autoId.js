2006732ab55658d42ff9e86128cccc6a71021568
Rick Buitrago <rick@elevatestartup.com>
2020-04-29 01:55:26 -0500

    creating actual random shrunkIds!

--- util/autoId.js
+++ util/autoId.js
@@ -1,6 +1,19 @@
 
+const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const number = '0123456789';
+
 const generateId = function() {
-    return 'abc' + '03';
+  let prefix = '';
+  for (let i = 0; i < 3; i++) {
+    prefix += alpha.charAt(Math.floor(Math.random() * alpha.length));
+  }
+  let suffix = '';
+  for (let i = 0; i < 2; i++) {
+    suffix += number.charAt(Math.floor(Math.random() * number.length));
+  }
+  
+  console.log('Generated random shrunk id; ' + prefix + suffix);
+  return prefix + suffix;
 }
 
 module.exports = {
