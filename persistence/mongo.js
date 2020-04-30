5511c57a34b56b1bdd816dd5168e662723c3291a
Rick Buitrago <rick@elevatestartup.com>
2020-04-29 23:40:58 -0500

    muting console!

--- persistence/mongo.js
+++ persistence/mongo.js
@@ -9,14 +9,14 @@ const retrieveStats = async function(query) {
       };
     const client = await mongo.connect(mongoConnector.dbUrl, options); 
     
-    console.log('Mongo Connection opened successfully');
+    // console.log('Mongo Connection opened successfully');
 
     const dbInstance = client.db('shrink');
-    console.log('Got connection to DB, now using "shrink" DB!');
+    // console.log('Got connection to DB, now using "shrink" DB!');
 
     const coll = await dbInstance.collection('client_data');
 
-    console.log('accessed collection! ' + coll.collectionName);
+    // console.log('accessed collection! ' + coll.collectionName);
 
     const results = await coll.find(query).toArray();
 
@@ -32,18 +32,18 @@ const insertClientData = async function(clientData) {
       };
     const client = await mongo.connect(mongoConnector.dbUrl, options); 
     
-    console.log('Mongo Connection opened successfully');
+    // console.log('Mongo Connection opened successfully');
 
     const dbInstance = client.db('shrink');
-    console.log('Got connection to DB, now using "shrink" DB!');
+    // console.log('Got connection to DB, now using "shrink" DB!');
 
     const coll = await dbInstance.collection('client_data');
 
-    console.log('accessed collection! ' + coll.collectionName);
+    // console.log('accessed collection! ' + coll.collectionName);
 
     const result = await coll.insertOne(clientData);
 
-    console.log('wrote ' + JSON.stringify(clientData) + ' to collection!;  objectId=' + result.insertedId);
+    // console.log('wrote ' + JSON.stringify(clientData) + ' to collection!;  objectId=' + result.insertedId);
 
     client.close();
 }
@@ -55,14 +55,14 @@ const shrinkAndReturn = async function(source) {
       };
     const client = await mongo.connect(mongoConnector.dbUrl, options); 
     
-    console.log('Mongo Connection opened successfully');
+    // console.log('Mongo Connection opened successfully');
 
     const dbInstance = client.db('shrink');
-    console.log('Got connection to DB, now using "shrink" DB!');
+    // console.log('Got connection to DB, now using "shrink" DB!');
 
     const coll = await dbInstance.collection('shrunk');
 
-    console.log('accessed collection! ' + coll.collectionName);
+    // console.log('accessed collection! ' + coll.collectionName);
 
     const shrunkData = {
         shrunkId: autoIdUtil.getNewShrunkId(),
@@ -70,7 +70,7 @@ const shrinkAndReturn = async function(source) {
     }
     const result = await coll.insertOne(shrunkData);
 
-    console.log('wrote ' + JSON.stringify(shrunkData) + ' to collection!;  objectId=' + result.insertedId);
+    // console.log('wrote ' + JSON.stringify(shrunkData) + ' to collection!;  objectId=' + result.insertedId);
 
     client.close();
     return shrunkData.shrunkId;
@@ -84,18 +84,18 @@ const updateShrunkEntry = async function(shrunkEntry) {
       };
     const client = await mongo.connect(mongoConnector.dbUrl, options); 
     
-    console.log('Mongo Connection opened successfully');
+    // console.log('Mongo Connection opened successfully');
 
     const dbInstance = client.db('shrink');
-    console.log('Got connection to DB, now using "shrink" DB!');
+    // console.log('Got connection to DB, now using "shrink" DB!');
 
     const coll = await dbInstance.collection('shrunk');
 
-    console.log('accessed collection! ' + coll.collectionName);
+    // console.log('accessed collection! ' + coll.collectionName);
 
     const result = await coll.findOneAndReplace({shrunkId: shrunkEntry.shrunkId}, shrunkEntry);
 
-    console.log('updated ' + JSON.stringify(shrunkEntry) + ' to collection!;  objectId=' + result.insertedId);
+    // console.log('updated ' + JSON.stringify(shrunkEntry) + ' to collection!;  objectId=' + result.insertedId);
 
     client.close();
     return shrunkEntry;
@@ -108,18 +108,18 @@ const findShrunkById = async function (shrunkId) {
         useUnifiedTopology: true,
       };
     const client = await mongo.connect(mongoConnector.dbUrl, options); 
-    console.log('Mongo Connection opened successfully');
+    // console.log('Mongo Connection opened successfully');
 
     const dbInstance = client.db('shrink');
-    console.log('Got connection to DB, now using "shrink" DB!');
+    // console.log('Got connection to DB, now using "shrink" DB!');
 
     const coll = await dbInstance.collection('shrunk');
 
-    console.log('accessed collection! ' + coll.collectionName);
+    // console.log('accessed collection! ' + coll.collectionName);
 
     const result = await coll.findOne({'shrunkId': shrunkId});
 
-    console.log('Found ' + JSON.stringify(result));
+    // console.log('Found ' + JSON.stringify(result));
 
     client.close();
     
